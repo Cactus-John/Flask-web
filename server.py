c2692afd74b236dec9ae4833207b8e7397354a4c
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2024-02-28 22:06:45 +0100

    check: data is correct and correctly shown on html pages

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
-from waitress import serve
 
 app = Flask(__name__)
 
@@ -11,26 +10,22 @@ def home():
 @app.route('/weather', methods = ['GET', 'POST'])
 def get_weather():
     city = request.args.get('city')
-
-    # Check for empty strings or string with only spaces
-    if not bool(city.strip()):
-        # You could render "City Not Found" instead like we do below
+    # Handlea prazan string ili razmake
+    if city is not None and not bool(city.strip()):
         city = "Samobor"
 
     weather_data = get_current_weather(city)
-
-    # City is not found by API
+    # Handlea error 404 (city not found); 200 -> success
     if not weather_data['cod'] == 200:
-        return render_template('city-not-found.html')
+        return render_template('city_not_found.html')
 
-    return render_template(
+    return render_template (
         "weather.html",
         title=weather_data["name"],
         status=weather_data["weather"][0]["description"].capitalize(),
         temp=f"{weather_data['main']['temp']:.1f}",
         feels_like=f"{weather_data['main']['feels_like']:.1f}"
     )
-
 
 if __name__ == "__main__":
     app.run(debug=True)
