e5b340df64d3005106e1e7214310f198eae3d3b8
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2024-02-28 22:20:01 +0100

    add waitress

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -28,4 +29,4 @@ def get_weather():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8000)
