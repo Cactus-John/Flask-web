2b6ac9e480176aa2f3db14a74201ff696cbc2af6
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2024-03-03 00:57:36 +0100

    music related

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
 from waitress import serve
+import pygame
 
 app = Flask(__name__)
 
@@ -12,9 +13,7 @@ def home():
 @app.route('/weather', methods = ['GET', 'POST'])
 
 def get_weather():
-
     city = request.args.get('city')
-
     # Handlea prazan string ili razmake
     if city is not None and not bool(city.strip()):
         city = "Samobor"
@@ -34,14 +33,64 @@ def get_weather():
         feels_like=f"{weather_data['main']['feels_like']:.1f}"
     )
 
-@app.route('/artists')
-def proba():
-    return render_template('artists.html')
+currently_playing = None
+is_paused = False
+
+def play_song(song_path):
+    pygame.mixer.init()
+
+    try:
+        print(f"Playing song: {song_path}")
+        pygame.mixer.music.load(song_path)
+        pygame.mixer.music.play()
+
+    except pygame.error as e:
+        print(f"Error playing song: {e}")
+
+
+@app.route('/artists', methods=['GET', 'POST'])
+
+def play_artist():
+    global currently_playing, is_paused
+    songs = ["LilUziVert_XO_tour_life.mp3",
+             "LilUziVert_20_Min.mp3",
+             "LilUziVert_That_Way.mp3"]
+
+    
+    if request.method == 'POST':
+        selected_song = request.form.get('selected_song')
+        song_path = f"static/songs/{selected_song}" 
+
+        if currently_playing and currently_playing.lower() == song_path.lower():
+            
+            if is_paused:
+                pygame.mixer.music.unpause()
+                is_paused = False
+                return f"Resumed playing: {selected_song}"
+            
+            else:
+                pygame.mixer.music.pause()
+                is_paused = True
+                return f"Paused: {selected_song}"
+        
+        else:
+            # Ako je selektirana druga pjesma, zaustavi ovu trenutnu
+            if currently_playing:
+                pygame.mixer.music.stop()
+            play_song(song_path)
+            currently_playing = song_path
+            is_paused = False
+            #return f"Playing: {selected_song}" 
+            current_song_name = selected_song
+            return render_template('artists.html', songs=songs, current_song_name=current_song_name)
+        
+    return render_template('artists.html', songs=songs, current_song_name=current_song_name)
 
 @app.route('/albums')
-def test():
+
+def test(): 
     return render_template('albums.html')
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    #serve(app, host="0.0.0.0", port=8000)
     app.run(debug=True)
