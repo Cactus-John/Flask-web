caaa8e75692a6b3c5c450e07e285c46b80f34b11
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2024-03-03 23:00:53 +0100

    .

--- server.py
+++ server.py
@@ -111,5 +111,5 @@ def test():
     return render_template('albums.html')
 
 if __name__ == "__main__":
-    #serve(app, host="0.0.0.0", port=8000)
+    serve(app, host="0.0.0.0", port=8000)
     app.run(debug=True)
