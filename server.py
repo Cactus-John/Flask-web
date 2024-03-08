53f8cc80700bcc5e94471f920e8e20bec661e100
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2024-03-09 00:12:05 +0100

    db

--- server.py
+++ server.py
@@ -2,18 +2,21 @@ from flask import Flask, render_template, request, url_for, redirect
 from weather import get_current_weather
 from waitress import serve
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 #--------------------------- DATABASE CREATION AND MANAGEMENT ----------------------------#
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SECRET_KEY'] = 'cactusjohn-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1523@localhost:5432/postgres'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
@@ -27,42 +30,60 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
-    
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+
     with app.app_context():
         db.create_all()
-    
-    
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[DataRequired(), Length(min=5, max=30)], render_kw={"placeholder": "Password"})
-    submit = SubmitField("Register")
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=35)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=20)])
+    submit = SubmitField('Sign Up')
     
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        
         if existing_user_username:
             raise ValidationError('That username already exists. Please choose a different one.')
-        
-    
-    
+
+
 class LoginForm(FlaskForm):
-    username = StringField(validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[DataRequired(), Length(min=5, max=30)], render_kw={"placeholder": "Password"})
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=35)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=20)])
     submit = SubmitField('Login')
+
+
+
+@app.route("/register", methods=['GET', 'POST'])
+
+def signup():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():   
+         
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for('login'))
     
+    return render_template('signup.html', title='Sign Up', form=form)
 
 
-@app.route('/login')
+
+@app.route("/login", methods=['GET', 'POST'])
+
 def login():
     form = LoginForm()
+    
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+        
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
@@ -70,34 +91,20 @@ def login():
             
     return render_template('login.html', form=form)
 
-      
-      
-@app.route('/success', methods=['GET', 'POST'])
-@login_required
-def success():
-    return render_template('success.html')
-   
+
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-    
-@app.route('/signup', methods=['POST', 'GET'])
-def signup():
-    form = RegisterForm()
 
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        
-        return redirect(url_for('login'))
+@app.route('/success', methods=['GET', 'POST'])
+@login_required
+def success():
+    return render_template('success.html')
+  
 
-    return render_template('signup.html', form=form)
-    
 #-----------------------------------------------------------------------------------------#
     
     
@@ -135,8 +142,7 @@ def get_weather():
 #-----------------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    #serve(app, host="0.0.0.0", port=8000)
     
-
+    #serve(app, host="0.0.0.0", port=8000)
     
     app.run(host="0.0.0.0", port=8000, debug=True)
